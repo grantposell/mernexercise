ef83832d9bebc0e176d8832e1754d35effdf5bab
grantposell <grantposell@gmail.com>
2019-05-27 20:53:50 -0500

    Connected server to MongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
