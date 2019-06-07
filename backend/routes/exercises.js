551bb80f9fa1ee7a27845db9edb23ef5fa7ffb83
grantposell <grantposell@gmail.com>
2019-06-06 23:03:50 -0500

    Added exercise route

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -1,3 +1,12 @@
+const router = require('express').Router();
+let Exercise = require('../models/exercise.model')
+
+router.route('/').get((req, res) => {
+    Exercise.find()
+    .then(exercises => res.json(exercises))
+    .catch (err => res.status(400).json('Error: ' + err));
+});
+
 router.route('/add').post((req, res) =>{
     const userName= req.body.userName;
     const description = req.body.description;
