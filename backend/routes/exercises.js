518df4471aaa432bc6f5492f8db67029a921b59d
grantposell <grantposell@gmail.com>
2019-06-10 21:04:23 -0500

    Add, update, and delete all done in backend

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -34,5 +34,18 @@ router.route('/:id').delete((req, res) => {
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/update/:id').post((req, res) => {
+    Exercise.findById(req.params.id)
+        .then(exercise => {
+            exercise.userName = req.body.userName;
+            exercise.description = req.body.description;
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date);
+            exercise.save()
+            .then(() => res.json('Exercise updated!'))
+            .catch(err => res.status(400).json('Error: ' + err));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+})
 
 module.exports = router;
