fdaaef2da6217db9475dc06056f552dc0a63e1c4
grantposell <grantposell@gmail.com>
2019-06-11 22:23:46 -0500

    Importing components that are about to be created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route} from "react-router-dom";
+import Navbar from "./components/navbar.component";
+import ExercisesList from "./components/exercises-list.component";
+import CreateExercise from "./components/create-exercise.component";
+import CreateUser from "./components/create-user.component";
 
 function App() {
   return (
