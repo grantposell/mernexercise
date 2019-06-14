2d635ca3df9a2ab647266b2ce3ae0fac2fa20983
grantposell <grantposell@gmail.com>
2019-06-13 21:46:30 -0500

    Added elements and connecting schema to database in create-exercise

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,6 +1,63 @@
 import React, { Component } from 'react';
 
 export default class CreateExercise extends Component{
+    constructor(props){
+        super(props);
+
+        this.onChangeUserName = this.onChangeUserName.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.onChangeDuration = this.onChangeDuration.bind(this);
+        this.onChangeDate = this.onChangeDate.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            userName: '',
+            description: '',
+            duration: 0,
+            date: new Date(),
+            users: []
+        }
+    }
+    componentDidMount(){
+        this.setState({
+            users: ['test user'],
+            userName: 'test user'
+        })
+    }
+
+    onChangeUserName(e){
+        this.setState({
+            userName: e.target.value
+        })
+    }
+    onChangeDescription(e){
+        this.setState({
+            description: e.target.value
+        })
+    }
+    onChangeDuration(e){
+        this.setState({
+            duration: e.target.value
+        })
+    }
+    onChangeDate(e){
+        this.setState({
+            date: date
+        })
+    }
+    onSubmit(e){
+        e.preventDefault();
+
+        const exercise = {
+            userName: this.state.userName,
+            description: this.state.description,
+            duration: this.state.duration,
+            date: this.state.date
+        }
+        console.log(exercise)
+        window.location = '/';
+    }
+
     render() {
         return (
             <div>
