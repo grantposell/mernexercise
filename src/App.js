dc856ca632f5002d88d3b72aeb5e0be782dd16ee
grantposell <grantposell@gmail.com>
2019-06-13 21:30:56 -0500

    Router to multiple crucial components working

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter as Router, Route} from "react-router-dom";
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
 import CreateExercise from "./components/create-exercise.component";
+import EditExercise from "./components/edit-exercise.component";
 import CreateUser from "./components/create-user.component";
+
 
 function App() {
   return (
