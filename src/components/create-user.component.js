6b04514c38bdf4f19899bbabe30d564c67830ee8
grantposell <grantposell@gmail.com>
2019-06-16 19:24:50 -0500

    Added axiosand fixing minor errors

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class CreateUser extends Component {
     constructor(props) {
@@ -23,6 +24,9 @@ export default class CreateUser extends Component {
             userName: this.state.userName,
         }
         console.log(user)
+
+        axios.post('http://localhost:5000/users/add', user)
+        .then(res => console.log(res.data));
 
         this.setState({
             userName: ''
