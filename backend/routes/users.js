2e22eb42ab9f13be5ec9e106a999eef04dd3b6e2
grantposell <grantposell@gmail.com>
2019-06-22 16:42:10 -0500

    Restructuring

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,16 +2,17 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
-    User.find()
+  User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
-    const userName = req.body.userName;
-    const newUser = new User({userName});
+  const username = req.body.username;
 
-    newUser.save()
+  const newUser = new User({username});
+
+  newUser.save()
     .then(() => res.json('User added!'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -2,51 +2,59 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 export default class CreateUser extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.onChangeUserName = this.onChangeUserName.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
 
-        this.state = {
-            userName: ''
-        }
+    this.state = {
+      username: ''
     }
-    onChangeUserName(e) {
-        this.setState({
-            userName: e.target.value
-        })
-    }
-    onSubmit(e) {
-        e.preventDefault();
+  }
 
-        const user = {
-            userName: this.state.userName,
-        }
-        console.log(user);
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value
+    })
+  }
 
-        axios.post('http://localhost:5000/users/add', user)
-        .then(res => console.log(res.data));
+  onSubmit(e) {
+    e.preventDefault();
 
-        this.setState({
-            userName: ''
-        })
+    const user = {
+      username: this.state.username
     }
-    render() {
-        return (
-            <div>
-                <h3>Create New Exercise Log</h3>
-                <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <label>Username: </label>
-                        <input type="text" required className="form-control" value={this.state.userName} onChange={this.onChangeUserName} />
-                    </div>
-                    <div className="form-group">
-                        <input type="submit" value="Create User" className="btn btn-primary">
-                        </input>
-                    </div>
-                </form>
-            </div >
-        )
-    }
+
+    console.log(user);
+
+    axios.post('http://localhost:5000/users/add', user)
+      .then(res => console.log(res.data));
+
+    this.setState({
+      username: ''
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group"> 
+            <label>Username: </label>
+            <input  type="text"
+                required
+                className="form-control"
+                value={this.state.username}
+                onChange={this.onChangeUsername}
+                />
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create User" className="btn btn-primary" />
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
